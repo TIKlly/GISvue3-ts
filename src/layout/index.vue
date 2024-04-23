dbddc9257f8dea9217cb427543cce1d8ff46f787
TIklly <1978931056@qq.com>
2024-04-24 01:54:09 +0800

    自适应  但存有bug

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,19 @@
 <template>
     <el-container>
-        <!-- 侧边栏 -->
-        <el-aside :width="sidebarWidth">
+        <!-- 侧边栏按钮（仅在移动端显示） -->
+        <el-button v-if="isMobile" type="primary" class="sidebar-btn" @click="toggleDrawer">
+            open
+        </el-button>
+        <!-- 侧边栏（移动端布局） -->
+        <el-drawer v-if="isMobile" v-model="isDrawerVisible" direction="ltr" :modal="false" >
+            <RootNav></RootNav>
+        </el-drawer>
+
+        <!-- 侧边栏（大屏幕布局） -->
+        <el-aside v-else>
             <RootNav></RootNav>
         </el-aside>
+
         <!-- 内容区域 -->
         <el-container>
             <el-header>
@@ -22,29 +32,40 @@ import RootNav from '@/components/Root/RootNav.vue';
 import RootHeader from "@/components/Root/RootHeader.vue"
 
 const isMobile = ref<boolean>(false)
+const isDrawerVisible = ref<boolean>(false)
 
 onMounted(() => {
     handleResize()
     window.addEventListener('resize', handleResize)
+    handleRouteChange();
 })
 
 function handleResize() {
-    isMobile.value = window.innerWidth < 768
+    isMobile.value = window.innerWidth < 767
 }
 
-// 使用计算属性动态计算侧边栏宽度和内容区域的内边距
-const sidebarWidth = computed(() => isMobile.value ? '100%' : '200px')
+function toggleDrawer() {
+    isDrawerVisible.value = !isDrawerVisible.value
+}
+
+// 根据当前的布局方式设置侧边栏的显示状态
+function handleRouteChange() {
+    if (isMobile.value) {
+        isDrawerVisible.value = false; // 在移动端布局中，路由跳转时隐藏侧边栏
+    }
+}
+
+// 使用计算属性动态计算内容区域的内边距
 const contentPadding = computed(() => isMobile.value ? '20px' : '40px')
-
-
 </script>
 
-<style lang="scss" scoped>
-/* 在移动端隐藏侧边栏 */
-.el-aside {
-    @media (max-width: 767px) {
-        display: block;
-        width: 100%;
-    }
+<style scoped lang="scss">
+.sidebar-btn {
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    font-size: 24px;
+    cursor: pointer;
+    z-index: 1000;
 }
 </style>
